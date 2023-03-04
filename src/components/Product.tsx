03b4127f3691ff036ba1bcf96197f39d8b9e5fbe
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2023-03-04 21:54:27 +0200

    added posibility to hide description of products

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IProduct } from './../models';
 
 interface ProductProps {
@@ -6,13 +6,28 @@ interface ProductProps {
 }
 
 export function Product({product}: ProductProps){
+    const [details, setDetails] = useState(false)
+
+    const btnClassName = details ? "bg-gray-300" : "bg-gray-500"
+    const commonStyleClassName = ["py-5 px-5 pb-2 border"]
+    const commonStyleClassNameWithoutBorders = ["py-2 px-2 pt-3 pb-3"]
+    const btnClasses = [commonStyleClassName, btnClassName]
 
     return (
-        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+        <div className={"rounded flex flex-col items-center mb-2" + commonStyleClassName.join(" ")}>
             <img src={product.image} className="w-1/6" alt={product.title} />
             <p>{product.title}</p>
             <span className="font-bold">{product.price}</span>
-            <p className="font-light">{product.description}</p>
+            <button 
+                style={{ borderRadius: '0.35rem' }}
+                className={"borderRadius: 0.25rem" + btnClasses.join(" ")}
+                onClick={() => setDetails(!details)}>
+                    {details ? "Hide details" : "Show details"}
+            </button>
+            
+            {details && <div>
+                <p className={"font-light" + commonStyleClassNameWithoutBorders.join(" ")}>{product.description}</p>
+            </div>}
         </div>
     )
 }
