5e9183452f9a4e238c00b4379c2f8a67755610a6
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2023-03-05 22:16:53 +0200

    added functionality of creating new test product

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-import { Error } from './components/Error';
-import { Product } from './components/Product';
-import { useProducts } from './hooks/products';
-import { Loading } from './components/Loading';
+import { Modal } from "./components/Modal";
+import { Error } from "./components/Error";
+import { Product } from "./components/Product";
+import { useProducts } from "./hooks/products";
+import { Loading } from "./components/Loading";
+import { CreateProduct } from "./components/CreateProduct";
+import { useState } from "react";
+import { IProduct } from "./models";
 
-function App() {  
+function App() {
+  const [visibilityModal, setVisibilityModal] = useState(true);
+  const { loading, products, error, addProduct } = useProducts();
 
-  const {loading, products, error} = useProducts()
+  const createHandler = (product: IProduct) => {
+    setVisibilityModal(false);
+    addProduct(product);
+  };
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      { loading && <Loading/> }
-      { error && <Error error={error} /> }
-      { products.map((product) => (
+      {loading && <Loading />}
+      {error && <Error error={error} />}
+      {products.map((product) => (
         <Product product={product} key={product.id} />
       ))}
+      {visibilityModal && (
+        <Modal
+          title="Create new product"
+          onClose={() => setVisibilityModal(false)}
+        >
+          <CreateProduct onCreate={createHandler} />
+        </Modal>
+      )}
+      <button 
+      className="fixed bottom-5 right-5 rounded-full bg-yellow-300 text-black text-2xl px-4 py-2"
+      onClick={() => {
+        setVisibilityModal(true)
+      }}>
+        +
+      </button>
     </div>
   );
 }
