79e2e2562e92d3085eb7038477783dc76352a355
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2023-03-05 12:40:44 +0200

    change local data to getting data via axiosGet

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import {Product} from "./components/Product";
-import { products } from './data/products';
+import { Error } from './components/Error';
+import { Product } from './components/Product';
+import { useProducts } from './hooks/products';
+import { Loading } from './components/Loading';
 
-function App() {
-  
+function App() {  
+
+  const {loading, products, error} = useProducts()
+
   return (
-      <div className="container mx-auto max-w-2xl pt-5">
-        <Product product={ products[0] } />
-        <Product product={ products[1] } />
-        <Product product={ products[1] } />
-        <Product product={ products[1] } />
-      </div>
-  )
+    <div className="container mx-auto max-w-2xl pt-5">
+      { loading && <Loading/> }
+      { error && <Error error={error} /> }
+      { products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
